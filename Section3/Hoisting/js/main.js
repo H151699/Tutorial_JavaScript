9f46c7b141127f2b704bfd33c90faffd644bed9b
Andrew Jianyou Day <andrewj.day@hotmail.com>
2018-01-28 22:57:44 +0100

    initial commit

---
+++ Section3/Hoisting/js/main.js
@@ -0,0 +1,26 @@
+﻿// Your code here!
+
+calculateAge(1985);
+
+function calculateAge(year) {
+    console.log(2018 - year);
+}
+
+
+var retirement = function (year) {
+    console.log(65 - (2018 - year));
+}
+
+retirement(1990);
+
+
+console.log(age);
+var age = 23;
+
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+
+foo();
+console.log(age);

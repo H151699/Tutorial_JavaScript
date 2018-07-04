1ff372af4b7cc3870b2285160a3eee67569f8253
Andrew Day <151699@stud.hvl.no>
2018-07-04 10:52:39 +0200

    add 'exit' function

---
+++ Section5/Coding Challenge 7/expertLevel of quizeApp/script.js
@@ -0,0 +1,108 @@
+
+
+/* After display the result,
+display the next random question, so that the game never ends
+(hint: write a funciton for this and call it right after displaying the result)*/
+
+/*
+  user writes'exit' instead of the anser to quite the quize
+*/
+
+
+/*
+  track the user's score to make the game more fun !
+  sp each time an anser is correct, and 1 point ot the score
+  hint: use the power of Closure for this,
+*/
+
+/*
+  Display the score in the console. use yet another method for this.
+*/
+
+
+(function(){ // avoid other code to override it
+
+
+
+
+
+
+
+
+function Question(question, answers, correct){
+
+  this.question = question;
+  this.answers = answers;
+  this.correct = correct;
+
+}
+
+Question.prototype.displayQuestion = function() {
+
+  console.log(this.question); // display questions
+
+  for(var i = 0; i < this.answers.length; i++ ){ // display answers
+    console.log(i + ':' + this.answers[i]);
+
+  }// forLoop
+
+}//prototype
+
+
+// check user's input answer.
+Question.prototype.checkAnswer = function(ans){
+  if(ans === this.correct){
+    console.log('Correct Answer hurra!');
+  }else{
+    console.log('Wrong answer, try again!');
+  }
+}
+
+
+
+
+// 2. create a couple of questions using the constructor
+
+var q1 = new Question('Is JavaScript the collest programming language in the world',
+                      ['YES', 'NO'],
+                      0
+                      );
+
+var q2 = new Question('What is the name of this course\'s teacher?',
+                      ['John', 'Micheal', 'Jianyou'],
+                      2
+                      );
+
+var q3 = new Question('What does best decribe coding',
+                      ['Boring', 'Hard', 'Fun', 'Tediuos'],
+                      2
+                      );
+
+
+
+var questionsArr = [q1, q2, q3]; // 3. store them all inside an array
+
+function nextQuestion(){
+
+
+
+var n = Math.floor(Math.random()*questionsArr.length);
+
+questionsArr[n].displayQuestion();
+
+var answer = prompt('Please select the correct answer.');
+
+
+if(answer !== 'exit'){
+
+  questionsArr[n].checkAnswer(parseInt(answer));
+  nextQuestion();
+
+}
+
+}// nextQuestion
+
+nextQuestion();
+
+
+})();
